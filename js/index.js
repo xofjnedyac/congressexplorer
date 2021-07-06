a43f6578cd5f2b99d6d6123cd2d5d8484c730384
Cayden <caydenfox12@outlook.com>
2021-07-05 23:00:09 -0600

    rendering congress tiles, functions for filtering by party

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@ function simplifyCongress(reps, sens) {
             state: rep.state,
             party: rep.party,
             title: rep.short_title,
+            imgUrl: `https:www.govtrack.us/static/legislator-photos/${rep.govtrack_id}-100px.jpeg`
 
         }
         
@@ -31,9 +32,10 @@ function simplifyCongress(reps, sens) {
             state: sen.state,
             party: sen.party,
             title: sen.short_title,
+            imgUrl: `https:www.govtrack.us/static/legislator-photos/${sen.govtrack_id}-100px.jpeg`    
 
         }
-        console.log(finalArray)
+ 
         finalArray.push(simpleSen)
 
     })
@@ -44,6 +46,57 @@ function simplifyCongress(reps, sens) {
 
 let newCongress =  simplifyCongress(representatives, senators);
 
-console.log(newCongress)
+const congressTiles = document.querySelector('#tileContainer')
 
+function renderCongress(congressArray) {
+    congressArray.forEach((rep) =>  {
+    const tile = document.createElement("div");
+    tile.setAttribute('class', 'tile');
+    const img = document.createElement("img");
+    img.setAttribute('src', rep.imgUrl);
+    const textHolder = document.createElement("div");
+    textHolder.setAttribute('class', 'name');
+    const text = document.createTextNode(rep.fullName);
+    textHolder.append(text);
+    tile.append(img);
+    tile.append(textHolder);
+    congressTiles.append(tile);
+})
+};
+renderCongress(newCongress);
 
+//sort
+function filterDems(congressArray) {
+    let filteredDems = [];
+    congressArray.forEach((rep) => {
+        if (rep.party === 'D')
+        filteredDems.push(rep)
+    })
+    return filteredDems;
+}
+
+const filteredDems = filterDems(newCongress);
+
+function filterRepubs(congressArray) {
+    let filteredRepubs = [];
+    congressArray.forEach((rep) => {
+        if (rep.party === 'R')
+        filteredRepubs.push(rep)
+    })
+    return filteredRepubs;
+}
+
+const filteredRepubs = filterRepubs(newCongress);
+
+function filterInds(congressArray) {
+    let filteredInds = [];
+    congressArray.forEach((rep) => {
+        if (rep.party === 'I')
+        filteredInds.push(rep)
+    })
+    return filteredInds;
+}
+
+const filteredInds = filterInds(newCongress);
+
+console.log(filteredInds)
